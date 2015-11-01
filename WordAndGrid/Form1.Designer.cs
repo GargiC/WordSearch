3ae4373a6a53e6dbd3c7b57db62978429d053547
Gargi D. Chakravarty <simpledotnetsolutions@gmail.com>
2015-11-01 12:56:46 -0500

    First commit

---
+++ WordAndGrid/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace WordAndGrid
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ddlAlign = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtX = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtY = new System.Windows.Forms.TextBox();
+            this.AddWord = new System.Windows.Forms.Button();
+            this.btnDone = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnExport = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(40, 202);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.dataGridView1.Size = new System.Drawing.Size(682, 326);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Word";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(131, 24);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(186, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Alignment";
+            // 
+            // ddlAlign
+            // 
+            this.ddlAlign.FormattingEnabled = true;
+            this.ddlAlign.Items.AddRange(new object[] {
+            "Horizontal",
+            "Vertical"});
+            this.ddlAlign.Location = new System.Drawing.Point(131, 52);
+            this.ddlAlign.Name = "ddlAlign";
+            this.ddlAlign.Size = new System.Drawing.Size(121, 21);
+            this.ddlAlign.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "X-Coordinate";
+            // 
+            // txtX
+            // 
+            this.txtX.Location = new System.Drawing.Point(131, 79);
+            this.txtX.MaximumSize = new System.Drawing.Size(100, 100);
+            this.txtX.Name = "txtX";
+            this.txtX.Size = new System.Drawing.Size(100, 20);
+            this.txtX.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(37, 105);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Y-Coordinate";
+            // 
+            // txtY
+            // 
+            this.txtY.Location = new System.Drawing.Point(131, 105);
+            this.txtY.MaximumSize = new System.Drawing.Size(100, 100);
+            this.txtY.Name = "txtY";
+            this.txtY.Size = new System.Drawing.Size(100, 20);
+            this.txtY.TabIndex = 8;
+            // 
+            // AddWord
+            // 
+            this.AddWord.Location = new System.Drawing.Point(40, 131);
+            this.AddWord.Name = "AddWord";
+            this.AddWord.Size = new System.Drawing.Size(75, 23);
+            this.AddWord.TabIndex = 9;
+            this.AddWord.Text = "Add Word";
+            this.AddWord.UseVisualStyleBackColor = true;
+            this.AddWord.Click += new System.EventHandler(this.AddWord_Click);
+            // 
+            // btnDone
+            // 
+            this.btnDone.Location = new System.Drawing.Point(140, 131);
+            this.btnDone.Name = "btnDone";
+            this.btnDone.Size = new System.Drawing.Size(75, 23);
+            this.btnDone.TabIndex = 10;
+            this.btnDone.Text = "Finish";
+            this.btnDone.UseVisualStyleBackColor = true;
+            this.btnDone.Click += new System.EventHandler(this.btnDone_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(245, 130);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 11;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(37, 167);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 13);
+            this.lblStatus.TabIndex = 12;
+            // 
+            // btnExport
+            // 
+            this.btnExport.Location = new System.Drawing.Point(338, 130);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(114, 23);
+            this.btnExport.TabIndex = 13;
+            this.btnExport.Text = "Export To CSV";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1030, 471);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnDone);
+            this.Controls.Add(this.AddWord);
+            this.Controls.Add(this.txtY);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtX);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.ddlAlign);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox ddlAlign;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtX;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtY;
+        private System.Windows.Forms.Button AddWord;
+        private System.Windows.Forms.Button btnDone;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button btnExport;
+    }
+}
+
---
+++ WordAndGrid/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WordAndGrid
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeDataGrid();
+        }
+
+        private void InitializeDataGrid()
+        {
+            //All Initialization stuff
+            dataGridView1.DefaultCellStyle.SelectionBackColor = dataGridView1.DefaultCellStyle.BackColor;
+            dataGridView1.DefaultCellStyle.SelectionForeColor = dataGridView1.DefaultCellStyle.ForeColor;
+
+            dataGridView1.ColumnHeadersVisible = false;
+            dataGridView1.RowHeadersVisible = false;
+            for(int i=0; i<15; i++)
+            {
+                dataGridView1.Columns.Add("Col" + i.ToString(), "");
+            }
+            dataGridView1.Rows.Add(15);
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+        }
+
+        private void FillGridWithRandomLetters()
+        {
+            //Fill up the grid's empty cells with random alphabets
+            Random rn = new Random();
+            char[] allLetters = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                for(int i=0; i< dataGridView1.ColumnCount; i++)
+                {
+                    if(row.Cells[i].Value == null)
+                    {
+                        int index = rn.Next(26);
+                        row.Cells[i].Value = allLetters[index];
+                        dataGridView1.Refresh();
+                    }
+                }
+            }
+            //No interactivity and read-only grid
+            dataGridView1.Enabled = false;
+            lblStatus.ForeColor = Color.Green;
+            lblStatus.Text = "All Done";
+        }
+
+
+        private void placeWordHoriz(string word, int row, int col)
+        {
+            //figure out if there are sufficient spots available
+            int wordLength = word.ToCharArray().Length;
+            int numOfSpots = dataGridView1.ColumnCount - col;
+            if(wordLength > numOfSpots)
+            {
+                //word is too long
+                lblStatus.ForeColor = Color.Red;
+                lblStatus.Text = "Word is too long";
+                return;
+            }
+            //place the word horizontally (left to right)
+            char[] wArray = word.ToCharArray();
+            for(int i=0; i<wArray.Length; i++)
+            {
+                dataGridView1.Rows[row].Cells[col+i].Value = wArray[i];
+                dataGridView1.Rows[row].Cells[col + i].Style.ForeColor = Color.Red;
+                dataGridView1.Rows[row].Cells[col + i].Style.Font = new Font(dataGridView1.Font, FontStyle.Bold);
+                dataGridView1.Refresh();
+            }
+        }
+
+        private void placeWordVert(string word, int row, int col)
+        {
+            //figure out if there are sufficient spots available
+            int wordLength = word.ToCharArray().Length;
+            int numOfSpots = dataGridView1.RowCount - row;
+            if (wordLength > numOfSpots)
+            {
+                //word is too long
+                lblStatus.ForeColor = Color.Red;
+                lblStatus.Text = "Word is too long";
+                return;
+            }
+            //place the word vertically (top to bottom)
+            char[] wArray = word.ToCharArray();
+            for (int i = 0; i < wArray.Length; i++)
+            {
+                dataGridView1.Rows[row+i].Cells[col].Value = wArray[i];
+                dataGridView1.Rows[row + i].Cells[col].Style.ForeColor = Color.Red;
+                dataGridView1.Rows[row + i].Cells[col].Style.Font = new Font(dataGridView1.Font, FontStyle.Bold);
+                dataGridView1.Refresh();
+            }
+        }
+
+        
+        private void AddWord_Click(object sender, EventArgs e)
+        {
+            lblStatus.Text = "";
+            //No validations here. Just grab the inputs
+            string word = textBox1.Text;
+            int align = ddlAlign.SelectedIndex;
+            int x = int.Parse(txtX.Text);
+            int y = int.Parse(txtY.Text);
+            if(align == 1)
+            {
+                //Vertically alligned
+                placeWordVert(word, y, x);
+            }
+            else
+            {
+                //Horizontally alligned
+                placeWordHoriz(word, y, x);
+            }
+            textBox1.Text = "";
+            ddlAlign.SelectedIndex = -1;
+            txtX.Text = "";
+            txtY.Text = "";
+        }
+
+        private void btnDone_Click(object sender, EventArgs e)
+        {
+            FillGridWithRandomLetters();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            //Re-initialize all the controls
+            lblStatus.Text = "";
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+            dataGridView1.ClearSelection();
+            InitializeDataGrid();
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            //Dump all the datagrid's content to a datatable
+            DataTable dt = new DataTable();
+            foreach (DataGridViewColumn column in dataGridView1.Columns)
+            {
+                dt.Columns.Add();
+            }
+            object[] cellValues = new object[dataGridView1.Columns.Count];
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                for (int i = 0; i < row.Cells.Count; i++)
+                {
+                    cellValues[i] = row.Cells[i].Value;
+                }
+                dt.Rows.Add(cellValues);
+            }
+            //Export the datatable
+            StringBuilder sb = new StringBuilder();
+            IEnumerable<string> columnNames = dt.Columns.Cast<DataColumn>().
+                                              Select(column => column.ColumnName);
+            sb.AppendLine(string.Join(",", columnNames));
+            foreach (DataRow row in dt.Rows)
+            {
+                IEnumerable<string> fields = row.ItemArray.Select(field => field.ToString());
+                sb.AppendLine(string.Join(",", fields));
+            }
+            File.WriteAllText("WordSearch.csv", sb.ToString());
+        }
+
+    }
+}
